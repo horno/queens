1cc516a1b651c4726874331e077a6ca9524c8d32
Horno <i.palacin.a@gmail.com>
2019-10-03 11:26:48 +0200

    Queens algorthm

---
+++ queens.py
@@ -0,0 +1,71 @@
+import sys
+
+  
+def printSol(board, N): 
+    for i in range(N): 
+        for j in range(N): 
+            print (board[i][j], end = " ") 
+        print() 
+  
+def fits(board, row, col, N): 
+  
+    for i in range(col): 
+        if board[row][i] == 1: 
+            return False
+  
+    for i, j in zip(range(row, -1, -1),  
+                    range(col, -1, -1)): 
+        if board[i][j] == 1: 
+            return False
+  
+    for i, j in zip(range(row, N, 1),  
+                    range(col, -1, -1)): 
+        if board[i][j] == 1: 
+            return False
+  
+    return True
+  
+def solveQueenRecursive(board, col, N): 
+      
+    if col >= N: 
+        return True
+  
+    for i in range(N): 
+  
+        if fits(board, i, col, N): 
+              
+            board[i][col] = 1
+  
+            if solveQueenRecursive(board, col + 1, N) == True: 
+                return True
+  
+            board[i][col] = 0
+  
+    return False
+  
+def solveQueen(): 
+    
+    if len(sys.argv) != 2:
+        print(" Use:\n\tpython3 queens.py N_of_NxN_table")
+        sys.exit()
+    N = int(sys.argv[1])
+    
+    board = []
+
+    for i in range(N):
+        board.append([])
+        for j in range(N):
+            board[i].append(0)
+
+    if solveQueenRecursive(board, 0, N) == False: 
+        print ("Solution does not exist") 
+        return False
+  
+    printSol(board, N) 
+    return True
+    
+
+
+
+  
+solveQueen() 
